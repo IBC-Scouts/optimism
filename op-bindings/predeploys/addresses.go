d76c32d918209297174abcd546a65efa9adb54fd
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2024-02-21 12:25:27 +0100

    imp: add basic ibc cross domain messenger contract

--- op-bindings/predeploys/addresses.go
+++ op-bindings/predeploys/addresses.go
@@ -26,6 +26,7 @@ const (
 	SchemaRegistry                = "0x4200000000000000000000000000000000000020"
 	EAS                           = "0x4200000000000000000000000000000000000021"
 	Create2Deployer               = "0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF2"
+	IBCCrossDomainMessenger       = "0x42000000000000000000000000000000000000E0" // ibc cross domain messenger predeploy
 	MultiCall3                    = "0xcA11bde05977b3631167028862bE2a173976CA11"
 	Safe_v130                     = "0x69f4D1788e39c87893C980c06EdF4b7f686e2938"
 	SafeL2_v130                   = "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA"
@@ -59,6 +60,7 @@ var (
 	SchemaRegistryAddr                = common.HexToAddress(SchemaRegistry)
 	EASAddr                           = common.HexToAddress(EAS)
 	Create2DeployerAddr               = common.HexToAddress(Create2Deployer)
+	IBCCrossDomainMessengerAddr       = common.HexToAddress(IBCCrossDomainMessenger)
 	MultiCall3Addr                    = common.HexToAddress(MultiCall3)
 	Safe_v130Addr                     = common.HexToAddress(Safe_v130)
 	SafeL2_v130Addr                   = common.HexToAddress(SafeL2_v130)
@@ -148,4 +150,6 @@ func init() {
 	for _, predeploy := range Predeploys {
 		PredeploysByAddress[predeploy.Address] = predeploy
 	}
+
+	Predeploys["IBCCrossDomainMessenger"] = &Predeploy{Address: IBCCrossDomainMessengerAddr}
 }
--- op-e2e/poc_test.go
+++ op-e2e/poc_test.go
@@ -2,16 +2,25 @@ package op_e2e
 
 import (
 	"context"
+	"math"
 	"math/big"
 	"testing"
 	"time"
 
+	"github.com/ethereum-optimism/optimism/op-bindings/bindings"
+	"github.com/ethereum-optimism/optimism/op-bindings/predeploys"
+	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/receipts"
+	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/transactions"
 	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/wait"
 	interceptornode "github.com/ethereum-optimism/optimism/op-e2e/interceptor-node"
+	"github.com/ethereum-optimism/optimism/op-node/rollup/derive"
+	"github.com/ethereum-optimism/optimism/op-service/testlog"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/stretchr/testify/require"
 )
 
@@ -82,4 +91,92 @@ func TestSendCosmosTx(t *testing.T) {
 	require.NoError(t, err)
 	cancel()
 	require.Equal(t, startNonce+1, endNonce, "Nonce of deposit sender should increment on L2, even if the deposit fails")
+}
+
+func TestIBCCrossChainMessenger(t *testing.T) {
+	InitParallel(t)
+
+	cfg := DefaultSystemConfig(t)
+
+	sys, err := cfg.Start(t)
+	require.Nil(t, err, "Error starting up system")
+	defer sys.Close()
+
+	log := testlog.Logger(t, log.LvlInfo)
+	log.Info("genesis", "l2", sys.RollupConfig.Genesis.L2, "l1", sys.RollupConfig.Genesis.L1, "l2_time", sys.RollupConfig.Genesis.L2Time)
+
+	l1Client := sys.Clients["l1"]
+	l2Client := sys.Clients["sequencer"]
+
+	opts, err := bind.NewKeyedTransactorWithChainID(sys.Cfg.Secrets.Alice, cfg.L1ChainIDBig())
+	require.Nil(t, err)
+
+	// Deploy WETH9
+	weth9Address, tx, WETH9, err := bindings.DeployWETH9(opts, l1Client)
+	require.NoError(t, err)
+	_, err = wait.ForReceiptOK(context.Background(), l1Client, tx.Hash())
+	require.NoError(t, err, "Waiting for deposit tx on L1")
+
+	// Get some WETH
+	opts.Value = big.NewInt(params.Ether)
+	tx, err = WETH9.Fallback(opts, []byte{})
+	require.NoError(t, err)
+	_, err = wait.ForReceiptOK(context.Background(), l1Client, tx.Hash())
+	require.NoError(t, err)
+	opts.Value = nil
+	wethBalance, err := WETH9.BalanceOf(&bind.CallOpts{}, opts.From)
+	require.NoError(t, err)
+	require.Equal(t, big.NewInt(params.Ether), wethBalance)
+
+	// Deploy L2 WETH9
+	l2Opts, err := bind.NewKeyedTransactorWithChainID(sys.Cfg.Secrets.Alice, cfg.L2ChainIDBig())
+	require.NoError(t, err)
+	optimismMintableTokenFactory, err := bindings.NewOptimismMintableERC20Factory(predeploys.OptimismMintableERC20FactoryAddr, l2Client)
+	require.NoError(t, err)
+	tx, err = optimismMintableTokenFactory.CreateOptimismMintableERC20(l2Opts, weth9Address, "L2-WETH", "L2-WETH")
+	require.NoError(t, err)
+	rcpt, err := wait.ForReceiptOK(context.Background(), l2Client, tx.Hash())
+	require.NoError(t, err)
+
+	event, err := receipts.FindLog(rcpt.Logs, optimismMintableTokenFactory.ParseOptimismMintableERC20Created)
+	require.NoError(t, err, "Should emit ERC20Created event")
+
+	// Approve WETH9 with the bridge
+	tx, err = WETH9.Approve(opts, cfg.L1Deployments.L1StandardBridgeProxy, new(big.Int).SetUint64(math.MaxUint64))
+	require.NoError(t, err)
+	_, err = wait.ForReceiptOK(context.Background(), l1Client, tx.Hash())
+	require.NoError(t, err)
+
+	// invoke cross domain messenger
+	ibcMessenger, err := bindings.NewIBCCrossDomainMessenger(predeploys.IBCCrossDomainMessengerAddr, l2Client)
+	require.NoError(t, err)
+	tx, err = transactions.PadGasEstimate(opts, 1.1, func(opts *bind.TransactOpts) (*types.Transaction, error) {
+		return ibcMessenger.SendMessage(opts, weth9Address, []byte("hello cosmos"), 100000)
+	})
+	require.NoError(t, err)
+	ibcMsgReceipt, err := wait.ForReceiptOK(context.Background(), l2Client, tx.Hash())
+	require.NoError(t, err)
+
+	t.Log("Message sent through IBCCrossDomainMessenger", "gas used", ibcMsgReceipt.GasUsed)
+
+	// compute the deposit transaction hash + poll for it
+	portal, err := bindings.NewOptimismPortal(cfg.L1Deployments.OptimismPortalProxy, l1Client)
+	require.NoError(t, err)
+
+	depositEvent, err := receipts.FindLog(ibcMsgReceipt.Logs, portal.ParseTransactionDeposited)
+	require.NoError(t, err, "Should emit deposit event")
+
+	depositTx, err := derive.UnmarshalDepositLogEvent(&depositEvent.Raw)
+	require.NoError(t, err)
+	_, err = wait.ForReceiptOK(context.Background(), l2Client, types.NewTx(depositTx).Hash())
+	require.NoError(t, err)
+
+	// Ensure that the deposit went through
+	optimismMintableToken, err := bindings.NewOptimismMintableERC20(event.LocalToken, l2Client)
+	require.NoError(t, err)
+
+	// Should have balance on L2
+	l2Balance, err := optimismMintableToken.BalanceOf(&bind.CallOpts{}, opts.From)
+	require.NoError(t, err)
+	require.Equal(t, l2Balance, big.NewInt(100))
 }
